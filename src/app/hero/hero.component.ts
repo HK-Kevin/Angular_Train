bdd8aa93cb1b8be3272e36939458b3f4e7ac6667
Jiaheng Du <dujiaheng@CZ1CG1.PRI>
2018-04-03 16:50:52 +0800

    Signed-off-by: fruitsandwich

---
+++ src/app/hero/hero.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero'
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent implements OnInit {
+
+   heros:Hero[]=[
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+  ];
+
+  hero: Hero;
+
+  handerClick(data){
+   this.hero = data;
+  }
+  constructor() { }
+
+
+  ngOnInit() {
+  }
+
+}
